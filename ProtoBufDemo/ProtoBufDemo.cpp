d1b36bc697e8f43270944273f7c8212356f602de
windpenguin <windpenguin@gmail.com>
2017-03-21 23:40:31 +0800

    add code

---
+++ ProtoBufDemo/ProtoBufDemo.cpp
@@ -0,0 +1,17 @@
+// ProtoBufDemo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "hello world" << endl;
+
+	system("pause");
+
+    return 0;
+}
+
